b7f330c49c2ff4cf3ee8ef78758df5e3dbf314a2
Your Name <you@example.com>
2022-02-08 14:42:03 +0100

    Startside, og enkelte elementer

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const app = express();
 
 
 // Er den korrekt i mappen? 
+
 const usercontroller = require("./source/controllers/users_controller");
 const productscontroller = require("./source/controllers/products_controller"); 
 
 
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT || 3000; 
 
 
 app.use(express.static("./source/client"));
@@ -21,7 +22,7 @@ app.use("/products", varecontroller);
 
 
 app.listen(PORT, ()=>{ 
-    console.log(`Server lytter på port ${PORT}`);
+    console.log(`Server is live on http://localhost:${PORT}`);
     });
 
     module.exports = app;
