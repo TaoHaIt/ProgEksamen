3fbe276048b0b6294a3358c899c792a6c0f0ce80
Your Name <you@example.com>
2022-02-09 10:28:16 +0100

    Succesfuldt inkoporeret elementer fra forrige projekt, første 6 krav er opfyldt

--- server.js
+++ server.js
@@ -1,28 +1,26 @@
 const express = require("express");
 const app = express();
 
+// Controllers
+//users
+const userController = require("./src/controllers/user-controller");
+
+// products
+const userProducts = require("./src/controllers/products-controller");
 
 
+const PORT = process.env.PORT || 3000;
 
-const usercontroller = require("./src/controllers/users_controller");
-const productscontroller = require("./src/controllers/products_controller"); 
-
-
-const PORT = process.env.PORT || 3000; 
-
-
+// Middleware
 app.use(express.static("./src/views"));
+JSON
 app.use(express.json());
 
+// Routes
+app.use("/users", userController);
+// route til marketplace, hvor brugere kan uploade varer
+app.use("/products", userProducts);
 
-app.use("/users", usercontroller); 
-app.use("/products", varecontroller); 
-
-
-
-
-app.listen(PORT, ()=>{ 
-    console.log(`Server is live on http://localhost:${PORT}`);
-    });
-
-    module.exports = app;
+// Start server
+app.listen(PORT, console.log(`Server is live on http://localhost:${PORT}`));
+ 
---
+++ src/views/productview.js
@@ -0,0 +1,71 @@
+
+ document.getElementById("clickMe").addEventListener('click', async () => {
+    let table = document.getElementById('generatedContent')
+ 
+    let result = await fetch(location.origin + "/products/get", { method: 'GET' })
+    .then(res => res.json())
+    .catch(err => console.log(err));
+   
+ 
+    let tableHTML = `
+    <tr>
+       <th>productName</th>
+       <th>price</th>
+       <th>userID</th>
+ 
+   </tr>
+    
+    `;
+ 
+    result.products.forEach(product => {
+     tableHTML += `
+       <tr>
+         <td>${product.productName}</td>
+         <td>${product.price}</td>
+         <td>${product.userID}</td>
+       </tr>
+     `;
+   });
+ 
+   table.innerHTML = tableHTML;
+ });
+  
+  
+  
+  /* document.addEventListener("DOMContentLoaded", (event) => { 
+   //vis alle varer
+   display.addEventListener('click', async () => {
+       let display = document.getElementById('display');
+       let tabel = document.getElementById('tabel');
+     tabel.innerHTML = `
+     <tr>
+       <th> Name </th>
+       <th> Price </th>
+       <th> UserID </th>
+     </tr>
+     `;
+    
+     await fetch ('http://localhost:3000/products/get', {
+       method: 'GET',
+       headers: {
+         "Content-Type": "application/json",
+       },
+     })
+     .then((res) => res.json())
+     .then((res) => {
+       console.log(res);
+    
+       res.forEach((e) => {
+         tabel.innerHTML += `
+         <tr>
+           <td> ${e.productName} </td>
+           <td> ${e.price} </td>
+           <td> ${e.userID} </td>
+         </tr>
+         `;
+       });
+     });
+   });
+   })
+   */
+  
